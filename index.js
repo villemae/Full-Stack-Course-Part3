343188871fd811eaafb8b907a01265ab54a5fd71
villemae <vileeni@hotmail.com>
2023-11-20 17:57:08 +0200

    3.2 done, info added

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+let notes = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+      },
+      {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+      },
+      {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+      },
+      {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+      }
+]
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(notes);
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date().toDateString();
+    console.log(date);
+    res.send(`<p> Phonebook has info for ${notes.length} people. </p>
+                <p> ${date} </p>`);
+})
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
+
