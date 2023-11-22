a2195c89454f42fb20a8c74c2dadbc1b4b9eede0
villemae <vileeni@hotmail.com>
2023-11-22 19:20:59 +0200

    Modified the PORT variable

--- index.js
+++ index.js
@@ -97,6 +97,6 @@ app.post('/api/persons', (req, res) => {
 // End of routes----------------
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
