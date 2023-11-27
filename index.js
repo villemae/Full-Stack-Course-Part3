38ba29e86ecef1597d1dfb43c5b4fd5e3f9e5c27
villemae <vileeni@hotmail.com>
2023-11-27 14:20:20 +0200

    3.15 - 3.18 done, rest of the operations work with MongoDB

--- index.js
+++ index.js
@@ -13,34 +13,10 @@ morgan.token('body', function getBody (req) {
 })
 
 // Middleware
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body[req]'));
 app.use(cors());
-app.use(express.static('dist'));
-
-// Hard-coded list of contacts
-let contacts = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-      },
-      {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-      },
-      {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-      },
-      {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-      }
-];
 
 // Routes---------------
 
@@ -49,10 +25,12 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
-app.get('/api/persons', (req, res) => {
-    Person.find({}).then( persons => {
+app.get('/api/persons', (req, res, next) => {
+    Person.find({})
+    .then( persons => {
         res.json(persons);
     })
+    .catch(error => next(error));
 })
 
 app.get('/info', (req, res) => {
@@ -65,27 +43,25 @@ app.get('/info', (req, res) => {
     
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id;
-    Person.findById(id)
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
     .then( person => {
         if (person) {
             res.json(person);
         } else {
             res.status(404).send(`Person with id ${id} not found!`);
         }
-        
     })
-    .catch((error) => {
-        console.log(error);
-        res.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    contacts = contacts.filter(contact => contact.id !== id);
-    res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+    .then( person => {
+        console.log(person);
+        res.status(204).end();
+    })
+    .catch(error => next(error));
 })
 
 app.post('/api/persons', (req, res) => {
@@ -102,14 +78,51 @@ app.post('/api/persons', (req, res) => {
             name: body.name,
             number: body.number,
         })
-        person.save().then(savedPerson => {
+        person.save()
+        .then(savedPerson => {
             res.json(savedPerson);
         })
+        .catch(error => next(error));
     }
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    console.log(req.body.number);
+    const person = {
+        name: req.body.name,
+        number: req.body.number,
+    };
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then( updatedContact => {
+        console.log(updatedContact);
+        res.json(updatedContact);
+    })
+    .catch(error => next(error));
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'});
+}
+
+app.use(unknownEndpoint);
+
 // End of routes----------------
 
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name == 'ReferenceError') {
+        return res.status(400).send({ error: 'id not defined' });
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler);
+  
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
